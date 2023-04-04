92384ead54086075ddd833fac704fdad61def35c
soohyang136 <jsh515228@gmail.com>
2023-04-04 14:31:31 +0900

    ADD :: mysql 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `./env/${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      entities: [__dirname + '/**/entity/*.entity.{js,ts}'],
+      logging: true,
+      timezone: 'local'
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
